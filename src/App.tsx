72787617df9319222fa89524b6fff3b92bf2a0ce
savannahharvey <ssammonstx@gmail.com>
2025-07-05 13:42:25 -0600

    retest

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,17 @@
-// import { Route, Routes } from "react-router-dom"
-// import { Home } from "./pages/Home";
-// import { NotFound } from "./pages/NotFound";
-
-// function App() {
-
-//   return (
-//     <>
-//         <Routes>
-//           <Route index element={<Home />}></Route>
-//           <Route path="*" element={<NotFound />}></Route>
-//         </Routes>
-//     </>
-//   )
-// }
-
-// export default App
-
+import { Route, Routes } from "react-router-dom"
+import { Home } from "./pages/Home";
+import { NotFound } from "./pages/NotFound";
 
 function App() {
-  return <h1>Hello GitHub Pages!</h1>
+
+  return (
+    <>
+        <Routes>
+          <Route index element={<Home />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+    </>
+  )
 }
+
+export default App
