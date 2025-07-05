19560b1b2e86caa6ee64dce83b0dd40bc9b28552
savannahharvey <ssammonstx@gmail.com>
2025-07-05 13:41:55 -0600

    test with simple app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
-import { Route, Routes } from "react-router-dom"
-import { Home } from "./pages/Home";
-import { NotFound } from "./pages/NotFound";
+// import { Route, Routes } from "react-router-dom"
+// import { Home } from "./pages/Home";
+// import { NotFound } from "./pages/NotFound";
+
+// function App() {
+
+//   return (
+//     <>
+//         <Routes>
+//           <Route index element={<Home />}></Route>
+//           <Route path="*" element={<NotFound />}></Route>
+//         </Routes>
+//     </>
+//   )
+// }
+
+// export default App
+
 
 function App() {
-
-  return (
-    <>
-        <Routes>
-          <Route index element={<Home />}></Route>
-          <Route path="*" element={<NotFound />}></Route>
-        </Routes>
-    </>
-  )
+  return <h1>Hello GitHub Pages!</h1>
 }
-
-export default App
