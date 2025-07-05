47ed3818167e568b7f430a4b113d4990e639277f
savannahharvey <ssammonstx@gmail.com>
2025-07-05 13:22:50 -0600

    fixed vite stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import { BrowserRouter, Route } from "react-router-dom"
-import { Routes } from "react-router-dom";
-
+import { Route, Routes } from "react-router-dom"
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
 
@@ -8,12 +6,10 @@ function App() {
 
   return (
     <>
-      <BrowserRouter>
         <Routes>
           <Route index element={<Home />}></Route>
           <Route path="*" element={<NotFound />}></Route>
         </Routes>
-      </BrowserRouter>
     </>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-// import path from "path";
 
-// https://vite.dev/config/
+
 export default defineConfig({
   base: '/portfolio_app/',
   plugins: [react(), tailwindcss()],
